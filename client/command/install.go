6a7eb0c58a288da6071fce754eed89d855a6d827
cander <cander@htdtek.com>
2020-12-20 10:55:47 +0800

    test install

--- client/command/install.go
+++ client/command/install.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"scs/client/cliconfig"
 	"scs/client/node"
 	"sync"
@@ -8,15 +9,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// install <-f yaml> || <-u url> || <name>
 var InstallCmd = &cobra.Command{
 	Use:   "install",
 	Short: "install package",
 	Long:  `install package`,
-	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		condition := 0
+		op := ""
+		if len(args) > 1 {
+			condition++
+			op = args[0]
+		}
+		if url != "" {
+			condition++
+			op = url
+		}
+		if file != "" {
+			condition++
+			op = file
+		}
+		if condition == 0 {
+			fmt.Println("at lease one params : scsctl install <-f yaml> || <-u url> || <name>")
+			return
+		}
+		if condition > 1 {
+			fmt.Println("only one params : scsctl install <-f yaml> || <-u url> || <name>")
+			return
+		}
 		if node.UseNodes != "" {
 			if nodeInfo, ok := cliconfig.Cfg.Nodes[node.UseNodes]; ok {
-				nodeInfo.Install(args[0], env)
+				nodeInfo.Install(op, env)
 				return
 			}
 		}
@@ -26,7 +49,7 @@ var InstallCmd = &cobra.Command{
 				if nodeInfo, ok := cliconfig.Cfg.Nodes[v]; ok {
 					wg.Add(1)
 					nodeInfo.Wg = wg
-					nodeInfo.Install(args[0], env)
+					nodeInfo.Install(op, env)
 				}
 			}
 			wg.Wait()
@@ -37,15 +60,19 @@ var InstallCmd = &cobra.Command{
 			nodeInfo.Name = name
 			wg.Add(1)
 			nodeInfo.Wg = wg
-			nodeInfo.Install(args[0], env)
+			nodeInfo.Install(op, env)
 		}
 		wg.Wait()
 
 	},
 }
 var env map[string]string
+var file string
+var url string
 
 func init() {
 	InstallCmd.Flags().StringToStringVarP(&env, "env", "e", nil, "set env")
+	InstallCmd.Flags().StringVarP(&file, "file", "f", "", "install from file")
+	InstallCmd.Flags().StringVarP(&url, "url", "u", "", "install from url")
 	rootCmd.AddCommand(InstallCmd)
 }
--- script/lookpath.go
+++ script/lookpath.go
@@ -2,25 +2,15 @@ package script
 
 import (
 	"os/exec"
-	"path/filepath"
-	"runtime"
 )
 
 func (s *Script) LookCommandPath() {
 	s.IsScript = true
 	for _, v := range s.LookPath {
-		path, err := exec.LookPath(v.Command)
+		_, err := exec.LookPath(v.Command)
 		if err != nil {
 			s.Start(v.Install)
 		}
-
-		dir := filepath.Dir(path)
-		if runtime.GOOS == "windows" {
-			s.Env["PATH"] += ";" + dir
-		} else {
-			s.Env["PATH"] += ":" + dir
-		}
-
 	}
 	s.IsScript = false
 }
