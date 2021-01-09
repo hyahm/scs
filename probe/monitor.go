d05d9a9495a7b10fe94ea5db11ed29af141df81f
cander <cander_liubiao@126.com>
2021-01-09 18:04:27 +0800

    修复多监控点出现的混乱报警

--- probe/monitor.go
+++ probe/monitor.go
@@ -11,31 +11,48 @@ import (
 	"github.com/hyahm/golog"
 )
 
+// var monitors Scan
+
+type Scan map[string]*Monitor
+
 type Monitor struct {
-	Monitor  []string
 	AI       *alert.AlertInfo
 	Interval time.Duration
 }
 
-func NewMonitor(monitor []string, interval, continuityInterval time.Duration) *Monitor {
-	return &Monitor{
-		Monitor: monitor,
-		AI: &alert.AlertInfo{
-			AM:                 &alert.Message{},
-			ContinuityInterval: continuityInterval,
-		},
-		Interval: interval,
+func NewMonitor(monitor []string, interval, continuityInterval time.Duration) Scan {
+	monitors := make(map[string]*Monitor)
+	for _, v := range monitor {
+		monitors[v] = &Monitor{
+			AI: &alert.AlertInfo{
+				AM:                 &alert.Message{},
+				ContinuityInterval: continuityInterval,
+			},
+			Interval: interval,
+		}
+	}
+	return monitors
+}
+
+func (m Scan) Update(probe *Probe) {
+	for _, v := range probe.Monitor {
+		if _, ok := m[v]; ok {
+			m[v].Interval = probe.Interval
+			m[v].AI.ContinuityInterval = probe.ContinuityInterval
+		} else {
+			m[v] = &Monitor{
+				AI: &alert.AlertInfo{
+					AM:                 &alert.Message{},
+					ContinuityInterval: probe.ContinuityInterval,
+				},
+				Interval: probe.Interval,
+			}
+		}
 	}
 }
 
-func (m *Monitor) Update(probe *Probe) {
-	m.Monitor = probe.Monitor
-	m.Interval = probe.Interval
-	m.AI.ContinuityInterval = probe.ContinuityInterval
-}
-
-func (m *Monitor) Check() {
-	for _, server := range m.Monitor {
+func (m Scan) Check() {
+	for server, mm := range m {
 		var failed bool
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
@@ -61,11 +78,11 @@ func (m *Monitor) Check() {
 		}
 
 		if failed {
-			m.AI.AM.HostName = server
-			m.AI.BreakDown(fmt.Sprintf("服务器或scs服务出现问题: %s", server))
+			mm.AI.AM.HostName = server
+			mm.AI.BreakDown(fmt.Sprintf("服务器或scs服务出现问题: %s", server))
 			continue
 		}
-		m.AI.Recover(fmt.Sprintf("服务器或scs服务恢复: %s", server))
+		mm.AI.Recover(fmt.Sprintf("服务器或scs服务恢复: %s", server))
 
 	}
 
