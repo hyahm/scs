0dbe323180f0019651d78e8c741aa3ce0053aa51
root <root@localhost.localdomain>
2021-01-10 12:16:18 +0800

    fix env

--- pkg/script/env.go
+++ pkg/script/env.go
@@ -3,7 +3,7 @@ package script
 func (s *Script) GetEnv() []string {
 	env := make([]string, 0, len(s.Env))
 	for k, v := range s.Env {
-		env = append(env, k+":"+v)
+		env = append(env, k+"="+v)
 	}
 	return env
 }
