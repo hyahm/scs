364b34f427b5b5f6e0aab28c36bc897456bb4663
cander <cander_liubiao@126.com>
2022-06-27 11:28:10 +0800

    修复linux cannotstop 处理无效的问题

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 # encoding=utf-8
 
-import imp
-from async_timeout import timeout
 from pyscs.scs import SCS
 from pyscs.atom import AtomSignal
 import time
@@ -14,12 +12,12 @@ scs = SCS()
 print(sys.argv, flush=True)
 
 while True:
-    print("can stop------", flush=True)
-    # atom = AtomSignal(timeout=10, notice=True, restart=True, parameter="aaaaa")
-    msg, code = scs.can_not_stop()
-    print(" can not stop------", flush=True)
-    time.sleep(random.randint(120, 200))
-    print(" can not stop------", flush=True)
+    
     msg, code =scs.can_stop()
-    time.sleep(random.randint(1, 3))
     print("can stop-----", flush=True)
+    time.sleep(random.randint(1, 3))
+    print(" can not stop------", flush=True)
+    atom = AtomSignal(timeout=5, notice=True, restart=True, parameter="aaaaa")
+    msg, code = scs.can_not_stop(atom=atom)
+    time.sleep(random.randint(120, 200))
+    
