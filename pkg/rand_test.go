079ac604fbbc590b058557a12a3b69d66495ec22
cander <cander_liubiao@126.com>
2024-11-01 10:51:26 +0800

    update rand

---
+++ pkg/rand_test.go
@@ -0,0 +1,10 @@
+package pkg
+
+import (
+	"testing"
+)
+
+func TestRand(t *testing.T) {
+	t.Error("dddddddddddddd")
+	t.Log(RandomToken())
+}
--- pkg/random.go
+++ pkg/random.go
@@ -8,11 +8,9 @@ import (
 func RandomToken() string {
 	s := `1234567890-=qwertyuiop[]asdfghjkl;zxcvbn#m,.!@%^&*()_+QWERTYUIOP{}ASDFGHJKL:|ZXCVBNM<>?`
 	out := ""
-	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(20)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	n := r.Intn(20)
 	for i := 0; i < n+30; i++ {
-		time.Sleep(time.Nanosecond * 1)
-		rand.Seed(time.Now().UnixNano())
 		r := rand.Intn(87)
 		out += s[r : r+1]
 	}
